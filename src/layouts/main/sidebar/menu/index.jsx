95dce58224efcc65738bce6f5c3af227bc212a27
Yakup Zengin <clafedersin@gmail.com>
2023-12-25 19:48:06 +0300

    added notification number

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -5,22 +5,27 @@ export default function Menu() {
 
     return (
         <nav className="mt-0.5 mb-1">
-            {mainMenu.map((menu,index) =>(
+            {mainMenu.map((menu, index) => (
                 <NavLink to={menu.path} key={index} className="py-1 block group">
-                {({ isActive }) => (
-                    <div className={classNames("p-3 rounded-full inline-flex items-center gap-5 text-xl group-hover:bg-[#eff3f41a]", {
-                        "font-bold": isActive
-                    })}>
-                        {!isActive && menu.icon.passive}
-    
-                        {isActive && menu.icon.active}
+                    {({ isActive }) => (
+                        <div className={classNames("p-3 rounded-full inline-flex items-center gap-5 text-xl group-hover:bg-[#eff3f41a]", {
+                            "font-bold": isActive
+                        })}>
+                            <div className="w-[26.25px] h-[26.25px] relative">
+                                <span className="w-[18px] h-[18px] rounded-full bg-[#1d9bf0] absolute -top-1 -right-1 flex items-center justify-center text-[10px]">4</span>
+                                {!isActive && menu.icon.passive}
 
-                        <div className="pr-4 text-xl">
-                            {menu.title}
+                                {isActive && menu.icon.active}
+
+
+                            </div>
+
+                            <div className="pr-4 text-xl">
+                                {menu.title}
+                            </div>
                         </div>
-                    </div>
-                )}
-            </NavLink>
+                    )}
+                </NavLink>
             ))}
         </nav>
     )
